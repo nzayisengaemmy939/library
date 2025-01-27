0befe49e438baffb587abb9280a5a4a68ceac41f
nzayisengaemmy939 <nzayisengaemmy2001@gmail.com>
2025-01-27 21:38:20 +0200

    adding some changes in downloading app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -87,7 +87,7 @@ export default function AccessibilityMenu({
           ))}
         </div>
 
-        <div className="flex items-center gap-1 mt-2 sm:mt-0 w-10">
+        <div className="flex items-center gap-1 mt-2 sm:mt-0">
           <span role="img" aria-hidden="true" className="text-xl">
             🌍
           </span>
@@ -101,7 +101,11 @@ export default function AccessibilityMenu({
         </div>
         <div className="mt-2">
           <div className="px-4 py-2 bg-blue-500 rounded-lg text-gray-200 ">
-            <a href="https://library-7aac0.web.app/app.apk" download>
+            <a
+              href="https://library-7aac0.web.app/app.apk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Download app
             </a>
           </div>
